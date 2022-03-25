42c2e3b8396ad5d40f9f317cfb505e277f9d66d5
israeloriente <israelnunesoriente@gmail.com>
2022-03-25 09:31:31 +0000

    feat: enable heroku main deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  VERSION: require('../../package.json').version,
+  Parse: {
+    serverURL: 'https://parseapi.back4app.com',
+    applicationId: 'bcB6XClP4d8vJHIZvcEUvxjeZsviag4gjP48IIfD',
+    jsKey: 'z46A36ynBeokVMbRjxxwO457t5t4o34lO58G9e4I',
+    masterKey: 'Never_Put_real_masterKey'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  VERSION: require('../../package.json').version,
+  Parse: {
+    serverURL: 'https://parseapi.back4app.com',
+    applicationId: 'bcB6XClP4d8vJHIZvcEUvxjeZsviag4gjP48IIfD',
+    jsKey: 'z46A36ynBeokVMbRjxxwO457t5t4o34lO58G9e4I',
+    masterKey: 'Never_Put_real_masterKey'
+  }
 };
 
 /*
